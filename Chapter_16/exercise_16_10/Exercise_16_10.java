3adb7d8836aa011a3b884d35853dd810c6d4c1aa
Vojnovits Csaba <csabavojnovits@yahoo.com>
2017-04-23 18:04:52 +0200

    commit Exercise 16_10

--- Chapter_16/exercise_16_10/Exercise_16_10.java
+++ Chapter_16/exercise_16_10/Exercise_16_10.java
@@ -1,20 +1,37 @@
 package exercise_16_10;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Exercise_16_10 extends Application {
     
     @Override
     public void start(Stage primaryStage) {
+        VBox pane = new VBox();
+        TextArea ta = new TextArea();
+        ta.setWrapText(true);
+        ta.setEditable(false);
+        ScrollPane scrollPane = new ScrollPane(ta);
         
+        HBox paneForFilename = new HBox();
+        TextField tfFilename = new TextField();
+        tfFilename.setAlignment(Pos.BOTTOM_LEFT);
+        tfFilename.setPrefColumnCount(17);
+        Button btView = new Button("View");
+        paneForFilename.getChildren().addAll(new Label("Filename"), tfFilename, btView);
         
-        Scene scene = new Scene(pane, 300, 250);
+        pane.getChildren().addAll(scrollPane, paneForFilename);
+        
+        Scene scene = new Scene(pane, 300, 150);
         primaryStage.setTitle("Text viewer");
         primaryStage.setScene(scene);
         primaryStage.show();
