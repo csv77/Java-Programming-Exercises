98e7ae943bef8f3494f8cd5f5af080a2af654885
vojnovitscs <csabavojnovits@yahoo.com>
2017-04-26 15:19:32 +0200

    commit Exercise_16_20

--- Chapter_16/exercise_16_20/Exercise_16_20.java
+++ Chapter_16/exercise_16_20/Exercise_16_20.java
@@ -1,24 +1,56 @@
 package exercise_16_20;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import teststopwatch.StopWatch;
 
 public class Exercise_16_20 extends Application {
+    private Button btControl = new Button("Start");
+    private Button btClear = new Button("Clear");
     
     @Override
     public void start(Stage primaryStage) {
         StopWatch stopWatch = new StopWatch();
+        HBox paneForButtons = new HBox(5);
+        paneForButtons.getChildren().addAll(btControl, btClear);
+        paneForButtons.setAlignment(Pos.CENTER);
         
+        BorderPane pane = new BorderPane();
+        pane.setCenter(stopWatch);
+        pane.setBottom(paneForButtons);
+        pane.setPadding(new Insets(5, 5, 5, 5));
         
+        btControl.setOnAction(e -> {
+            if(btControl.getText() == "Start" || btControl.getText() == "Resume") {
+                btControl.setText("Pause");
+                stopWatch.play();
+            }
+            else if(btControl.getText() == "Pause") {
+                btControl.setText("Resume");
+                stopWatch.pause();
+            }
+        });
         
-        Scene scene = new Scene(pane, 300, 250);
+        btClear.setOnAction(e -> {
+            btControl.setText("Start");
+            stopWatch.clear();
+        });
+        
+        Scene scene = new Scene(pane);
         primaryStage.setTitle("Stopwatch");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-
+    
     public static void main(String[] args) {
         launch(args);
     }
---
+++ Chapter_16/exercise_16_20/StopWatch.java
@@ -0,0 +1,59 @@
+package exercise_16_20;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.geometry.Insets;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.util.Duration;
+
+public class StopWatch extends StackPane {
+    private int hour;
+    private int min;
+    private int sec;
+    private Timeline animation;
+    private Text text = new Text();
+    
+    public StopWatch() {
+        setPadding(new Insets(15, 5, 15, 5));
+        clear();
+        getChildren().add(text);
+        text.setFont(Font.font(30));
+        animation = new Timeline(new KeyFrame(Duration.millis(10), e -> {
+            run();
+        }));
+        animation.setCycleCount(Timeline.INDEFINITE);
+    }
+    
+    public void play() {
+        animation.play();
+    }
+    
+    public void pause() {
+        animation.pause();
+    }
+    
+    public void run() {
+        if(min == 59) {
+            hour++;
+            min = 0;
+        }
+        if(sec == 59) {
+            min++;
+        }
+        sec = sec < 59 ? sec + 1 : 0;
+        text.setText(getTime());
+    }
+    
+    public void clear() {
+        hour = 0;
+        min = 0;
+        sec = 0;
+        text.setText(getTime());
+    }
+    
+    public String getTime() {
+        return String.format("%02d:%02d:%02d", hour, min, sec);
+    }
+}
