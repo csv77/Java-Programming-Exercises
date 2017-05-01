bbfc7a26c5fb4d319f438cc351151ad471e1356f
Vojnovits Csaba <csabavojnovits@yahoo.com>
2017-05-01 10:12:44 +0200

    mod. Exercise_17_03

--- Chapter_17/exercise_17_03/Exercise_17_03.java
+++ Chapter_17/exercise_17_03/Exercise_17_03.java
@@ -11,29 +11,36 @@ import java.io.IOException;
 
 public class Exercise_17_03 {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         int sum = 0;
         File file = new File("Chapter_17/Exercise17_03.dat");
         
-        if(!file.exists()) {
-            //create Exercise17_03.dat file containing unspecified number of integers
-            try(DataOutputStream output = new DataOutputStream(new FileOutputStream(file))) {
-                for(int i = 0; i < (int)(Math.random() * 100); i++) {
-                    output.writeInt((int)(Math.random() * 100));
+        try {
+            if(!file.exists()) {
+                //create Exercise17_03.dat file containing unspecified number of integers
+                try(DataOutputStream output = new DataOutputStream(new FileOutputStream(file))) {
+                    for(int i = 0; i < (int)(Math.random() * 100); i++) {
+                        output.writeInt((int)(Math.random() * 100));
+                    }
+                }
+            }
+
+            //read integers from Exercise_17_03.dat to get the sum of the numbers in the file
+            try(DataInputStream input = new DataInputStream(new FileInputStream("Chapter_17/Exercise17_03.dat"))) {
+                int read;
+                while((read = input.readInt()) != -1) {
+                    sum += input.readInt();
                 }
             }
         }
-        
-        //read integers from Exercise_17_03.dat to get the sum of the numbers in the file
-        try(DataInputStream input = new DataInputStream(new FileInputStream("Chapter_17/Exercise17_03.dat"))) {
-            int count = input.available() / 4;
-            while(count > 0) {
-                sum += input.readInt();
-                count--;
-            }
+        catch(EOFException e) {
+            System.out.println("All data were read.");
+        }
+        catch(IOException e) {
+            e.printStackTrace();
         }
         
-        System.out.println("The sum of the number is file Exercise17_03.dat: " + sum);
+        System.out.println("The sum of the number in Exercise17_03.dat: " + sum);
     }
     
 }
