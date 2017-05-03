aeecaedda3933c8064e2d2514a1fe91d06162c6c
Vojnovits Csaba <csabavojnovits@yahoo.com>
2017-05-03 21:59:47 +0200

    commit Exercise_17_14 and 17_15

---
+++ Chapter_17/exercise_17_14/Exercise_17_14.java
@@ -0,0 +1,33 @@
+package exercise_17_14;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Exercise_17_14 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input filename: ");
+        String inputFile = scanner.next();
+        System.out.print("Name of encrypted file: ");
+        String outputFile = scanner.next();
+        
+        try {
+            try(BufferedInputStream input = new BufferedInputStream(new FileInputStream("Chapter_17/" + inputFile));
+                BufferedOutputStream output = new BufferedOutputStream(new FileOutputStream("Chapter_17/" + outputFile))) {
+                int value;
+                while((value = input.read()) != -1) {
+                    output.write(value + 5);
+                }
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+}
