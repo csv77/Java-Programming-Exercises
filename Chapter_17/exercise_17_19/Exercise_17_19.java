0c71bb8756fdf771e78770bf01a16aeed378d08b
Vojnovits Csaba <csabavojnovits@yahoo.com>
2017-05-05 16:20:53 +0200

    mod. Exercises_17_19

---
+++ Chapter_17/exercise_17_19/Exercise_17_19.java
@@ -0,0 +1,28 @@
+package exercise_17_19;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Exercise_17_19 {
+
+    public static void main(String[] args) {
+        System.out.print("Enter the filename: ");
+        String fileName = new Scanner(System.in).nextLine();
+        String binary = "";
+        
+        try {
+            try(BufferedInputStream input = new BufferedInputStream(new FileInputStream("Chapter_17/" + fileName))) {
+                int value;
+                while((value = input.read()) != -1) {
+                    System.out.print(Integer.toHexString(value).toUpperCase());
+                }
+            }
+            System.out.println(binary);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
