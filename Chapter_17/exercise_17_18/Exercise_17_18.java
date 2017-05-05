90b53b40f0e9e7fcf146d08009ac24ee8c04b1b9
Vojnovits Csaba <csabavojnovits@yahoo.com>
2017-05-05 15:54:03 +0200

    commit Exercises_17_18

---
+++ Chapter_17/exercise_17_18/Exercise_17_18.java
@@ -0,0 +1,38 @@
+package exercise_17_18;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Exercise_17_18 {
+
+    public static void main(String[] args) {
+        System.out.print("Enter the filename: ");
+        String fileName = new Scanner(System.in).nextLine();
+        String binary = "";
+        
+        try {
+            try(BufferedInputStream input = new BufferedInputStream(new FileInputStream("Chapter_17/" + fileName))) {
+                int value;
+                while((value = input.read()) != -1) {
+                    binary += getBits(value) + " ";
+                }
+            }
+            System.out.println(binary);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+    }
+    
+    public static String getBits(int value) {
+        String bits = "";
+        for(int i = 0; i < 8; i++) {
+            bits = ((value >> i) & 1) + bits;
+        }
+        return bits;
+    }
+}
