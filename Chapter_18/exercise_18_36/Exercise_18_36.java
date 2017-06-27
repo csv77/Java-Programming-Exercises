61c60fcd4b2fb960f8f96095f47520285e416ba5
Vojnovits Csaba <csabavojnovits@yahoo.com>
2017-06-27 21:35:12 +0200

    commit Exercise_18_36

---
+++ Chapter_18/exercise_18_36/Exercise_18_36.java
@@ -0,0 +1,98 @@
+package exercise_18_36;
+
+import javafx.application.Application;
+import javafx.geometry.Point2D;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Polygon;
+import javafx.stage.Stage;
+
+public class Exercise_18_36 extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {       
+        SierpinskiTrianglePane pane = new SierpinskiTrianglePane(); 
+        
+        HBox hBox = new HBox(10);
+        TextField tfOrder = new TextField("0");
+        tfOrder.setAlignment(Pos.CENTER_RIGHT);
+        tfOrder.setPrefColumnCount(4);
+        hBox.getChildren().addAll(new Label("Enter an order:"), tfOrder);
+        hBox.setAlignment(Pos.CENTER);
+                
+        tfOrder.setOnAction(e -> pane.setOrder(Integer.parseInt(tfOrder.getText())));
+
+        BorderPane borderPane = new BorderPane();
+        borderPane.setCenter(pane);
+        borderPane.setBottom(hBox);
+
+        Scene scene = new Scene(borderPane, 200, 210);
+        primaryStage.setTitle("SierpinskiTriangle");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        pane.widthProperty().addListener(ov -> pane.paint());
+        pane.heightProperty().addListener(ov -> pane.paint());
+        pane.setOrder(0);
+        
+    }
+
+    static class SierpinskiTrianglePane extends Pane {
+        private int order = 0;
+
+        public int getOrder() {
+            return order;
+        }
+
+        public void setOrder(int order) {
+            this.order = order;
+            paint();
+        }
+
+        SierpinskiTrianglePane() {
+        }
+
+        protected void paint() {
+            Point2D p1 = new Point2D(getWidth() / 2, 10);
+            Point2D p2 = new Point2D(10, getHeight() - 10);
+            Point2D p3 = new Point2D(getWidth() - 10, getHeight() - 10);
+
+            this.getChildren().clear(); 
+
+            displayTriangles(order, p1, p2, p3);
+        }
+
+        private void displayTriangles(int order, Point2D p1, 
+            Point2D p2, Point2D p3) {
+            if (order == 0) {
+                Polygon triangle = new Polygon();
+                triangle.getPoints().addAll(p1.getX(), p1.getY(), p2.getX(), 
+                    p2.getY(), p3.getX(), p3.getY());
+                triangle.setStroke(Color.BLACK);
+                triangle.setFill(Color.BLACK);
+
+                this.getChildren().add(triangle);
+            } 
+            else {
+                Point2D p12 = p1.midpoint(p2);
+                Point2D p23 = p2.midpoint(p3);
+                Point2D p31 = p3.midpoint(p1);
+
+                displayTriangles(order - 1, p1, p12, p31);
+                displayTriangles(order - 1, p12, p2, p23);
+                displayTriangles(order - 1, p31, p23, p3);
+            }
+        }
+    }
+  
+    public static void main(String[] args) {
+        launch(args);
+    }    
+}
