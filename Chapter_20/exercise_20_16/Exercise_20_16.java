1f248c6c7abf64d5c2039e004dde4f470307a04f
vojnovitscs <csabavojnovits@yahoo.com>
2017-07-14 13:44:05 +0200

    commit Exercises_20_16

--- Chapter_20/exercise_20_16/Exercise_20_16.java
+++ Chapter_20/exercise_20_16/Exercise_20_16.java
@@ -1,48 +1,79 @@
 package exercise_20_16;
 
-import java.util.LinkedList;
 import java.util.Stack;
 
 public class Exercise_20_16 {
 
     public static void main(String[] args) {
-        
+        System.out.println("Infix Expression\tPostfix Expression");
+	System.out.println("(1 * 2 + 2) * 3\t\t" + infixToPostfix("(1 * 2 + 2) * 3"));
+	System.out.println("2 * (1 * 2 + 3)\t\t" + infixToPostfix("2 * (1 * 2 + 3)"));
     }
     
     public static String infixToPostfix(String expression) {
         String[] tokens = insertBlanks(expression).split(" ");
-        LinkedList<String> operators = new LinkedList<>();
-        LinkedList<String> operands = new LinkedList<>();
-        Stack<Character> parentheses = new Stack<>();
+        Stack<Character> operatorStack = new Stack<>();
+        String result = "";
         
         for(String token : tokens) {
             if(token.length() == 0) {
                 continue;
             }
+            if(Character.isDigit(token.charAt(0))) {
+                result += token;
+            }
             if(token.charAt(0) == '(') {
-                parentheses.push(token.charAt(0));
+                operatorStack.push(token.charAt(0));
             }
-            else if(!parentheses.empty() && parentheses.peek() == '(' && token.charAt(0) != ')') {
-                if(Character.isDigit(token.charAt(0))) {
-                    operands.addLast(token);
+            if(token.charAt(0) == ')') {
+                while(operatorStack.peek() != '(') {
+                    result += operatorStack.pop();
                 }
-                else if() {
-                    
-                }
-                
+                operatorStack.pop();
             }
-            
-            
+            if(operatorStack.isEmpty() || operatorStack.peek() == '(') {
+                if(isOperator(token.charAt(0))) {
+                    operatorStack.push(token.charAt(0));
+                }
+            }
+            else if(token.charAt(0) == '*' || token.charAt(0) == '/') {
+                if(!operatorStack.isEmpty() && (operatorStack.peek() == '+' || operatorStack.peek() == '-')) {
+                    operatorStack.push(token.charAt(0));
+                }
+                else if(!operatorStack.isEmpty() && (operatorStack.peek() == '*' || operatorStack.peek() == '/')) {
+                    result += operatorStack.pop();
+                    operatorStack.push(token.charAt(0));
+                }
+            }
+            else if(token.charAt(0) == '+' || token.charAt(0) == '-') {
+                if(!operatorStack.isEmpty() && (operatorStack.peek() == '+' || operatorStack.peek() == '-')) {
+                    result += operatorStack.pop();
+                    operatorStack.push(token.charAt(0));
+                }
+                else if(operatorStack.peek() == '*' || operatorStack.peek() == '/') {
+                    while(!operatorStack.isEmpty() && (operatorStack.peek() == '*' || operatorStack.peek() == '/')) {
+                        result += operatorStack.pop();
+                    }
+                    operatorStack.push(token.charAt(0));
+                }
+            }
         }
-        
-        
+        while(!operatorStack.isEmpty()) {
+            result += operatorStack.pop();
+        }
+        return result;
+    }
+    
+    public static boolean isOperator(char ch) {
+        return ch == '*' || ch == '/' || ch == '+' || ch == '-';
     }
     
     public static String insertBlanks(String s) {
         String result = "";
         for(int i = 0; i < s.length(); i++) {
             if(s.charAt(i) == '+' || s.charAt(i) == '-' ||
-                    s.charAt(i) == '*' || s.charAt(i) == '/') {
+                    s.charAt(i) == '*' || s.charAt(i) == '/' ||
+                    s.charAt(i) == '(' || s.charAt(i) == ')') {
                 result += " " + s.charAt(i) + " ";
             }
             else {
