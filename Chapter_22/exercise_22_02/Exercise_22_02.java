ef12da2b9b2b48850627aace88d3ba6f6c972a51
Vojnovits Csaba <csabavojnovits@yahoo.com>
2017-07-26 18:42:33 +0200

    commit Exercise_22_02

--- Chapter_22/exercise_22_02/Exercise_22_02.java
+++ Chapter_22/exercise_22_02/Exercise_22_02.java
@@ -1,5 +1,7 @@
 package exercise_22_02;
 
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Exercise_22_02 {
@@ -7,17 +9,28 @@ public class Exercise_22_02 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter a string: ");
-        String string = input.next();
+        String string = input.nextLine();
         
-        char[] array = string.toCharArray();
-        String substring = "" + array[0];
+        LinkedList<Character> maxSubstring = new LinkedList<>();
         
-        for(int i = 1; i < array.length; i++) {
-            if(substring.charAt(substring.length() - 1) <= array[i]) {
-                substring += "" + array[i];
+        for(int i = 0; i < string.length(); i++) {
+            LinkedList<Character> substring = new LinkedList<>(Arrays.asList(string.charAt(i)));
+            for(int j = i + 1; j < string.length(); j++) {
+                if(substring.getLast() <= string.charAt(j)) {
+                    substring.add(string.charAt(j));
+                }
             }
+            
+            if(maxSubstring.size()< substring.size()) {
+                maxSubstring.clear();
+                maxSubstring.addAll(substring);
+            }
+            substring.clear();
         }
         
-        System.out.println(substring);
+        for(char ch : maxSubstring) {
+            System.out.print(ch);
+        }
+        System.out.println();
     }
 }
