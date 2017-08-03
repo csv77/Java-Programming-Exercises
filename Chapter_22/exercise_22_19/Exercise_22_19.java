fc73e73e4938f9af51ce1a820c9ea132cc9c46f5
vojnovitscs <csabavojnovits@yahoo.com>
2017-08-03 09:02:17 +0200

    commit Exercise_22_19

--- Chapter_22/exercise_22_19/Exercise_22_19.java
+++ Chapter_22/exercise_22_19/Exercise_22_19.java
@@ -13,12 +13,12 @@ import javafx.stage.Stage;
 
 public class Exercise_22_19 extends Application {
     private static final int N = 10;
-    private TextField[][] tfMatrix = new TextField[N][N];
     
     @Override
     public void start(Stage primaryStage) {
+        TextField[][] tfMatrix = new TextField[N][N];
         BorderPane borderPane = new BorderPane();
-        GridPane gridPane = fillMatrix();
+        GridPane gridPane = fillMatrix(tfMatrix);
         
         Button btRefresh = new Button("Refresh");
         Button btFindLargestBlock = new Button("Find Largest Block");
@@ -35,6 +35,16 @@ public class Exercise_22_19 extends Application {
             for(int i = 0; i < N; i++) {
                 for(int j = 0; j < N; j++) {
                     tfMatrix[i][j].setText("" + (int)(Math.random() * 2));
+                    tfMatrix[i][j].setStyle(null);
+                }
+            }
+        });
+        
+        btFindLargestBlock.setOnAction(e -> {
+            int[] blockInfo = findLargestBlock(tfMatrix);
+            for(int i = blockInfo[0]; i < blockInfo[0] + blockInfo[2]; i++) {
+                for(int j = blockInfo[1]; j < blockInfo[1] + blockInfo[2]; j++) {
+                    tfMatrix[i][j].setStyle("-fx-background-color: lightgrey");
                 }
             }
         });
@@ -44,12 +54,45 @@ public class Exercise_22_19 extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-
-    public static void main(String[] args) {
-        launch(args);
+    
+    public int[] findLargestBlock(TextField[][] tfMatrix) {
+        int[] blockInfo = new int[3];
+        int[][] matrix = new int[N][N];
+        for(int i = 0; i < N; i++) {
+            matrix[0][i] = Integer.parseInt(tfMatrix[0][i].getText());
+        }
+        
+        for(int i = 0; i < N; i++) {
+            matrix[i][0] = Integer.parseInt(tfMatrix[i][0].getText());
+        }
+                
+        for(int i = 1; i < N; i++) {
+            for(int j = 1; j < N; j++) {
+                if(Integer.parseInt(tfMatrix[i][j].getText()) == 0) {
+                    matrix[i][j] = 0;
+                }
+                else if(Integer.parseInt(tfMatrix[i][j].getText()) == 1) {
+                    int min = Math.min(matrix[i - 1][j], matrix[i - 1][j - 1]);
+                    matrix[i][j] = Math.min(min, matrix[i][j - 1]) + 1;
+                }
+            }
+        }
+        
+        blockInfo[2] = 0;
+        for(int i = 1; i < N; i++) {
+            for(int j = 1; j < N; j++) {
+                if(blockInfo[2] <= matrix[i][j]) {
+                    blockInfo[2] = matrix[i][j];
+                    blockInfo[0] = i - blockInfo[2] + 1;
+                    blockInfo[1] = j - blockInfo[2] + 1;
+                }
+            }
+        }
+        
+        return blockInfo;
     }
     
-    public GridPane fillMatrix() {
+    public GridPane fillMatrix(TextField[][] tfMatrix) {
         GridPane pane = new GridPane();
         for(int i = 0; i < N; i++) {
             for(int j = 0; j < N; j++) {
@@ -60,5 +103,9 @@ public class Exercise_22_19 extends Application {
             }
         }
         return pane;
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
     }
 }
