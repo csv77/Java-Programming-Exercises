06c565fa10d5a88b836fa2dc35362894626c0ee7
Vojnovits Csaba <csabavojnovits@yahoo.com>
2017-08-17 12:54:46 +0200

    commit Exercise_23_12

---
+++ Chapter_23/exercise_23_12/Exercise_23_12.java
@@ -0,0 +1,50 @@
+package exercise_23_12;
+
+import java.util.ArrayList;
+
+public class Exercise_23_12 {
+    private static final int N = 7;
+    
+    public static void main(String[] args) {
+        int[] list = new int[1000000];
+        for(int i = 0; i < list.length; i++) {
+            list[i] = (int)(Math.random() * Math.pow(10, N));
+        }
+        
+        for(Integer i : list) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        
+        radixSort(list, N);
+        for(Integer i : list) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+    
+    public static void radixSort(int[] list, int numberOfDigits) {
+        ArrayList<Integer>[] buckets = new ArrayList[10];
+        for(int i = 0; i < buckets.length; i++) {
+            buckets[i] = new ArrayList<>();
+        }
+        
+        for(int i = 0; i < numberOfDigits; i++) {
+            for(int j = 0; j < buckets.length; j++) {
+                buckets[j].clear();
+            }
+            
+            for(int j = 0; j < list.length; j++) {
+                int temp = (list[j] / (int)(Math.pow(10, i))) % 10;
+                buckets[temp].add(list[j]);
+            }
+            
+            int k = 0;
+            for(int j = 0; j < buckets.length; j++) {
+                for(int n = 0; n < buckets[j].size(); n++) {
+                    list[k++] = buckets[j].get(n);
+                }
+            }
+        }
+    }
+}
