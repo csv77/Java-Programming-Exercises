05273293b09fb6b0db0cb35e1bf7e96a90566471
Vojnovits Csaba <csabavojnovits@yahoo.com>
2017-09-12 21:42:00 +0200

    commit Exercise_28_01

--- Chapter_28/exercise_28_01/Exercise_28_01.java
+++ Chapter_28/exercise_28_01/Exercise_28_01.java
@@ -3,7 +3,6 @@ package exercise_28_01;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Exercise_28_01 {
