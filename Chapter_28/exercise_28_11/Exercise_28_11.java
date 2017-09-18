74684c84c09fcef7e43e5d5ed266fa7765464247
Vojnovits Csaba <csabavojnovits@yahoo.com>
2017-09-18 22:29:09 +0200

    commit Exercise_28_11

--- Chapter_28/exercise_28_11/Exercise_28_11.java
+++ Chapter_28/exercise_28_11/Exercise_28_11.java
@@ -1,7 +1,11 @@
 package exercise_28_11;
 
+import java.util.List;
 import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -11,13 +15,15 @@ import javafx.scene.layout.HBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
-import sun.font.FontFamily;
 
 public class Exercise_28_11 extends Application {
+    private NineTailModel model = new NineTailModel();
+    private NineTailPane pane = new NineTailPane();
     
     @Override
     public void start(Stage primaryStage) {
         BorderPane borderPane = new BorderPane();
+        borderPane.setPadding(new Insets(2));
         HBox hBoxForButtons = new HBox(5);
         hBoxForButtons.setAlignment(Pos.CENTER);
         
@@ -26,29 +32,46 @@ public class Exercise_28_11 extends Application {
         hBoxForButtons.getChildren().addAll(btSolve, btStart);
         borderPane.setBottom(hBoxForButtons);
         
-        NineTailModel model = new NineTailModel();
-        char[] nodes = {'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H'};
-        NineTailPane pane = new NineTailPane(nodes);
-        
         HBox hBoxForNodes = new HBox(5);
         hBoxForNodes.getChildren().add(pane);
-        hBoxForNodes.setStyle("-fx-border-color : red");
+        hBoxForNodes.setStyle("-fx-border-color : blue");
+        hBoxForNodes.setPadding(new Insets(2));
         borderPane.setCenter(hBoxForNodes);
         
-        Scene scene = new Scene(borderPane, 300, 140);
+        Scene scene = new Scene(borderPane, 300, 150);
         primaryStage.setTitle("Exercise_28_11");
         primaryStage.setScene(scene);
         primaryStage.show();
+        
+        btSolve.setOnAction(e -> {
+            hBoxForNodes.getChildren().clear();
+            hBoxForNodes.getChildren().add(pane);
+            pane.setInitialNode();
+            List<Integer> path = model.getShortestPath(NineTailModel.getIndex(pane.node));
+            for(int i = 1; i < path.size(); i++) {
+                char[] node = NineTailModel.getNode(path.get(i));
+                char[] previousNode = NineTailModel.getNode(path.get(i - 1));
+                NineTailPane nextPane = new NineTailPane(node, previousNode);
+                hBoxForNodes.getChildren().add(nextPane);
+            }
+            primaryStage.setWidth((path.size() + 1) * pane.getWidth());
+            
+        });
+        
+        btStart.setOnAction(e -> {
+            hBoxForNodes.getChildren().clear();
+            pane = new NineTailPane();
+            hBoxForNodes.getChildren().add(pane);
+        });
     }
     
     public class NineTailPane extends GridPane {
-        private char[] node;
+        private char[] node = new char[9];
+        private char[] previousNode = new char[9];
         
-
-        public NineTailPane(char[] node) {
-            this.node = node;
+        public NineTailPane() {
             for(int i = 0; i < 9; i++) {
-                Label lbHOrT = new Label(node[i] + "");
+                Label lbHOrT = new Label("H");
                 lbHOrT.setFont(Font.font("Courier New", FontWeight.BOLD, 30));
                 this.add(lbHOrT, i % 3, i / 3);
                 lbHOrT.setOnMouseClicked(e -> {
@@ -58,11 +81,31 @@ public class Exercise_28_11 extends Application {
                     else {
                         lbHOrT.setText("H");
                     }
-                });            }
+                });
+            }
             this.setStyle("-fx-border-color : black");
         }
         
+        public NineTailPane(char[] node, char[] previousNode) {
+            this.node = node;
+            this.previousNode = previousNode;
+            for(int i = 0; i < 9; i++) {
+                Label lbHOrT = new Label(node[i] + "");
+                if(node[i] != previousNode[i]) {
+                    lbHOrT.setStyle("-fx-text-fill : red");
+                }
+                lbHOrT.setFont(Font.font("Courier New", FontWeight.BOLD, 30));
+                this.add(lbHOrT, i % 3, i / 3);
+            }
+            this.setStyle("-fx-border-color : black");
+        }
         
+        public void setInitialNode() {
+            ObservableList<Node> list = this.getChildren();
+            for(int i = 0; i < list.size(); i++) {
+                node[i] = ((Label)list.get(i)).getText().charAt(0);
+            }
+        }
     }
 
     public static void main(String[] args) {
