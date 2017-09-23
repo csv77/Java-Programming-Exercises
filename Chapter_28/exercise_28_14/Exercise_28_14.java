28e23476f7452c71d56854f42deea937d4da3c3d
Vojnovits Csaba <csabavojnovits@yahoo.com>
2017-09-23 22:53:16 +0200

    commit Exercise_28_14

--- Chapter_28/exercise_28_14/Exercise_28_14.java
+++ Chapter_28/exercise_28_14/Exercise_28_14.java
@@ -1,11 +1,11 @@
 package exercise_28_14;
 
-import exercise_28_13.NineTailModel4By4;
+import exercise_28_13.TailModel;
 
 public class Exercise_28_14 {
 
     public static void main(String[] args) {
-        NineTailModel4By4 model = new NineTailModel4By4();
+        TailModel model = new TailModel();
         int numberOfVertices = model.tree.getNumberOfVerticesFound();
         System.out.println("The number of starting nodes that have a solution: " + numberOfVertices);
         System.out.println("The number of starting nodes that don't have a solution: " + (model.NUMBER_OF_NODES - numberOfVertices));
