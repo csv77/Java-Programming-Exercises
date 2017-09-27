b284c956496fb7fd67c46f6befdc85fd9f54a86f
Vojnovits Csaba <csabavojnovits@yahoo.com>
2017-09-27 22:19:46 +0200

    commit Exercise_28_15

--- Chapter_28/exercise_28_15/Exercise_28_15.java
+++ Chapter_28/exercise_28_15/Exercise_28_15.java
@@ -39,7 +39,7 @@ public class Exercise_28_15 extends Application {
         
         Scene scene = new Scene(borderPane, 300, 200);
         //scene.getStylesheets().add(exercise_28_11.Exercise_28_11.class.getResource("style.css").toExternalForm());
-        scene.getStylesheets().add("file:///c:/programming/java/Intro_to_Java_Programming_10th_exercises/JavaProgrammingExercises/Chapter_28/exercise_28_11/style.css");
+        scene.getStylesheets().add("file:///c:/egyéb/SZE/Intro_to_Java_Programming_10th_exercises/Java-Programming-Exercises/Chapter_28/exercise_28_11/style.css");
         primaryStage.setTitle("Exercise_28_15");
         primaryStage.setScene(scene);
         primaryStage.show();
